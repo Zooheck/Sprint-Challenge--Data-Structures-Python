1db1c58860cb05d077ef2701683bf5dadab594fb
Zooheck <davidscottflack@gmail.com>
2019-05-10 10:31:38 -0500

    Solve append method

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,16 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        if self.current < self.capacity:
+            self.storage[self.current] = item
+            self.current += 1
+        elif self.current == self.capacity:
+            self.storage.append(item)
+            self.storage.pop(0)
 
-  def get(self):
-    pass
+    def get(self):
+        return self.storage
